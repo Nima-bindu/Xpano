c597e0f8cbc0a31e2fa2540af89a6c0af86a0c81
Nima-bindu <91862306+Nima-bindu@users.noreply.github.com>
2022-04-16 15:03:02 +0530

    Update script.js

--- Xpano_extensionn/script.js
+++ Xpano_extensionn/script.js
@@ -1,4 +1,4 @@
-let API_BASE_URL="http://127.0.0.1:5000"
+let API_BASE_URL="https://gmail.googleapis.com"
 console.warn("YESHSHHHHH")
 console.warn(document.title)
 const aTags = document.getElementsByTagName("a")
