7f355e1485ae7b2dc17bca40b10f726bbdab46ea
Nima-bindu <91862306+Nima-bindu@users.noreply.github.com>
2022-04-16 22:33:37 +0530

    Update popup.js

--- Xpano_extensionn/popup.js
+++ Xpano_extensionn/popup.js
@@ -1,4 +1,4 @@
-let API_BASE_URL="https://gmail.googleapis.com"
+let API_BASE_URL="http://127.0.0.1:5000"
 
 document.addEventListener('DOMContentLoaded', function() {
     var scannerBtn = document.getElementById('scanner');
